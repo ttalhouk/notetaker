d9171dedb1c232e5c112633053d37eeffbc657c6
Talal Talhouk <talal@sbcglobal.net>
2016-08-25 14:11:22 -0700

    add proptype validation

--- app/components/GitHub/UserProfile.js
+++ app/components/GitHub/UserProfile.js
@@ -2,6 +2,10 @@ const React = require('react');
 
 
 const UserProfile = React.createClass({
+  propTypes:{
+    bio: React.PropTypes.object.isRequired,
+    username: React.PropTypes.string.isRequired
+  },
   render: function(){
     console.log(this.props);
     return (
