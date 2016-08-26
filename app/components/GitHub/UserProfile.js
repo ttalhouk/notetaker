80b7c6205a886620c92c7a4715adc71be7f51188
Talal Talhouk <talal@sbcglobal.net>
2016-08-25 20:02:43 -0700

    add github profile info

--- app/components/GitHub/UserProfile.js
+++ app/components/GitHub/UserProfile.js
@@ -10,8 +10,16 @@ const UserProfile = React.createClass({
     console.log(this.props);
     return (
       <div>
-        <h2>User Profile: {this.props.username}</h2>
-        <p>Bio: {this.props.bio.name} </p>
+        {this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
+        {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
+        {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
+        {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
+        {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
+        {this.props.bio.company && <li className="list-group-item">Company: {this.props.bio.company}</li>}
+        {this.props.bio.followers && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
+        {this.props.bio.following && <li className="list-group-item">Following: {this.props.bio.following}</li>}
+        {this.props.bio.following && <li className="list-group-item">Public Repos: {this.props.bio.public_repos}</li>}
+        {this.props.bio.blog && <li className="list-group-item">Blog: <a href={this.props.bio.blog}> {this.props.bio.blog}</a></li>}
       </div>
     )
   }
---
+++ app/utils/helpers.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + "/repos");
+};
+
+function getUserInfo(username) {
+  return axios.get('https://api.github.com/users/' + username);
+};
+
+var helpers = {
+  getGitHubInfo: function(username) {
+    return axios.all([getRepos(username), getUserInfo(username)])
+      .then(function(arr){
+        return {
+          repos: arr[0].data,
+          bio: arr[1].data
+        }
+      });
+  }
+};
+
+module.exports = helpers;
